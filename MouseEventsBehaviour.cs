464b4376a8fee66a3e2844a557ca60b0d5e0d847
Cameron Bass <Cameronjudd98@gmail.com>
2022-08-31 16:30:17 -0600

    created mouse events behaviour script

---
+++ MouseEventsBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace unityTools {
+    public class MouseEventsBehaviour : MonoBehaviour {
+        public UnityEvent onMouseDownEvent,
+            onMouseDragEvent,
+            onMouseEnterEvent,
+            onMouseExitEvent,
+            onMouseUpEvent,
+            onMouseUpAsButtonEvent;
+
+        private void OnMouseDown() {
+            onMouseDownEvent.Invoke();
+        }
+
+        private void OnMouseDrag() {
+            onMouseDragEvent.Invoke();
+        }
+
+        private void OnMouseEnter() {
+            onMouseEnterEvent.Invoke();
+        }
+
+        private void OnMouseExit() {
+            onMouseExitEvent.Invoke();
+        }
+//don't use OnMouseOver, too expensive
+        private void OnMouseUp() {
+            onMouseUpEvent.Invoke();
+        }
+
+        private void OnMouseUpAsButton() {
+            onMouseUpAsButtonEvent.Invoke();
+        }
+    }
+}
