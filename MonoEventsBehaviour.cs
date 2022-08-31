9595614f5f2c9f8e53d99437c8f2c02d412499d4
Cameron Bass <Cameronjudd98@gmail.com>
2022-08-31 16:01:02 -0600

    created mono events behaviour script

---
+++ MonoEventsBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace unityTools {
+    public class MonoEventsBehaviour : MonoBehaviour {
+        public UnityEvent startEvent, awakeEvent, disableEvent;
+
+        private void Awake() {
+            awakeEvent.Invoke();
+        }
+
+        private void Start() {
+            startEvent.Invoke();
+        }
+
+        private void OnDisable() {
+            disableEvent.Invoke();
+        }
+    }
+}
