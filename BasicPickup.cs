1ffdac38b418647300c8faaf1150bbf1b09b600b
Cameron Bass <cameronjudd98@gmail.com>
2022-09-13 11:40:13 -0600

    cleaning pickups

--- BasicPickup.cs
+++ BasicPickup.cs
@@ -1,19 +1,15 @@
-﻿namespace unityTools {
+﻿using UnityEngine.Events;
+
+namespace unityTools {
     public class BasicPickup: InteractableBase {
+        public UnityEvent pickupEvent;
         private void Start() {
             rangeSphere.isTrigger = true;
         }
 
         protected override void OnRangeEnter() {
             base.OnRangeEnter();
-        }
-
-        protected override void OnRangeExit() {
-            base.OnRangeExit();
-        }
-
-        protected override void InRangeUpdate() {
-            base.InRangeUpdate();
+            pickupEvent.Invoke();
         }
     }
 }
