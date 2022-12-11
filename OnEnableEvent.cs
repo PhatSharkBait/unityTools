36e85d176d7eb9eb39629a4e4cc2d54acd9ebdb8
Cameron Bass <Cameronjudd98@gmail.com>
2022-12-11 01:01:32 -0700

    OnEnableEvent

---
+++ OnEnableEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace unityTools {
+    public class OnEnableEvent : MonoBehaviour {
+        public UnityEvent enableEvent;
+
+        private void OnEnable() {
+            enableEvent.Invoke();
+        }
+    }
+}
